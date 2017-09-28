f9eca16addc3ce9d781e38de2d6c353ff49d958a
kloomis1 <kloomis@algorafinance.com>
2017-09-27 22:17:13 -0500

    Add blackScholes.R for options pricing

---
+++ blackScholes.R
@@ -0,0 +1,39 @@
+# blackScholes.R
+# Black-Scholes options pricing 
+# Author: Kyle Loomis
+# Date Updated: 9/27/17
+# Summary: Stores all functions necessary to
+#          calculate options price
+
+# install.packages("fOptions")
+# install.packages("quantmod")
+library(fOptions)
+library(quantmod)
+
+# log returns - returns without NA values
+logReturns <- function(priceTS) {
+  return(na.omit(TTR::ROC(priceTS)))
+}
+
+# annualized volatility
+calcVol <- function(priceTS) {
+  return((sd(priceTS) * sqrt(250) * 100)/100)
+}
+
+# general black scholes model to calclate 
+# call or put options 
+blackScholes <- function(priceTS, type, strike, days, rf, carryCost) {
+  GBSOption(TypeFlag = type, S = as.numeric(tail(priceTS, n=1)), X = strike, Time = days/365, 
+            r = rf, b = carryCost, sigma = calcVol(logReturns(priceTS)),
+            title = NULL, description = NULL)
+}
+
+# NVDA price
+getSymbols("NVDA")
+# 1 year worth of closed prices
+price <- NVDA$NVDA.Close["2016-09-26::"]
+
+# assumes risk-free rate of 0.0123
+blackScholes(priceTS = price, type = "c", strike = 180.00, days = 1, 
+             rf = 0.0123, carryCost = 0.0)
+
