34279c4fb6e3ec9a5195a40cf93a00fcbb14a51a
kloomis1 <kloomis@algorafinance.com>
2017-10-09 17:23:57 -0500

    Adding framework for VaR project

---
+++ VaR.R
@@ -0,0 +1,16 @@
+# VaR: Value at Risk
+# Author: Kyle Loomis
+# Date Updated: 10/4/17
+# Desc: Project Outline - Value at Risk 
+
+# necessary library
+library(quantmod)
+
+# Data: Fetch -> Clean (use log returns) -> Visualize
+# use NVDA prices from Yahoo
+
+# Model: Write function for model
+
+# Testing: Test specified time periods 
+
+# Conclusions and cleanup
